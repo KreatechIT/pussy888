6d09bf72e1e3717420820e65567535b04ba34202
Santanu Datta <santanudatta94@gmail.com>
2025-09-08 14:29:54 +0600

    home update

--- resources/views/livewire/home-livewire.blade.php
+++ resources/views/livewire/home-livewire.blade.php
@@ -1,18 +1,1 @@
-@push('title')
-    Pussy888 Malaysia: Download, Play Slots & Live Casino | Trusted Online Casino
-@endpush
-
-@push('meta_title')
-    Pussy888 Malaysia | Download & Play Trusted Online Casino
-@endpush
-
-@push('meta_description')
-    Download & play! Enjoy top slots & live casino games at a trusted online platform in Malaysia. Find your favorite games
-    now!
-@endpush
-
-@push('meta_keywords')
-    Pussy888, Pussy888 Malaysia, Download Pussy888, Online Casino Malaysia, Live Casino Malaysia, Slot Games Malaysia
-@endpush
-
 @include('livewire.home.home_' . app()->getLocale())
--- resources/views/livewire/home/home_en.blade.php
+++ resources/views/livewire/home/home_en.blade.php
@@ -1,3 +1,19 @@
+@push('title')
+    Pussy888 Malaysia: Download, Play Slots & Live Casino | Trusted Online Casino
+@endpush
+
+@push('meta_title')
+    Pussy888 Malaysia | Download & Play Trusted Online Casino
+@endpush
+
+@push('meta_description')
+    Download & play! Enjoy top slots & live casino games at a trusted online platform in Malaysia. Find your favorite games
+    now!
+@endpush
+
+@push('meta_keywords')
+    Pussy888, Pussy888 Malaysia, Download Pussy888, Online Casino Malaysia, Live Casino Malaysia, Slot Games Malaysia
+@endpush
 <div>
     <!-- Hero -->
     <div class="relative">
